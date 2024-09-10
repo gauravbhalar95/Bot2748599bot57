89230c9736b5bc965fb412bf1aa8c25b1499d51f
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2024-09-10 22:58:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def webhook_handler():
 @app.route('/')
 def webhook():
     bot2.remove_webhook()
-    bot2.set_webhook(url='https://your-render-url.onrender.com/' + API_TOKEN_2, timeout=60)
+    bot2.set_webhook(url='https://bot2-mb9e.onrender.com/' + API_TOKEN_2, timeout=60)
     return "Webhook set", 200
 
 if __name__ == "__main__":
