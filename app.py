b4ac1fb73688cd812ce0e519d4c6663db0df36fe
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2024-09-25 22:07:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -192,5 +192,5 @@ def webhook():
     return "Webhook set", 200
 
 if __name__ == "__main__":
-    # Run the Flask app
-    app.run(host='0.0.0.0', port=80)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
