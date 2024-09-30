604c3fe70763e13e3a4f3fdec918ae92602d27af
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2024-09-30 19:28:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -137,4 +137,4 @@ def webhook():
 
 if __name__ == "__main__":
     # Run the Flask app in debug mode
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
