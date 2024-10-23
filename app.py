279080f3ddfe71e2c36e6ff31daa43166661ff2d
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2024-10-23 22:01:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def sanitize_filename(filename, max_length=200):
 # yt-dlp options optimized for speed
 def get_ydl_opts():
     return {
-        'format': 'bestvideo[height<=1080]+bestaudio/best[height<=1080]'
+        'format': 'best[ext=mp4]/best'
 ,  # Best quality
         'outtmpl': f'{output_dir}%(title)s.%(ext)s',  # Save path for media files
         'cookiefile': cookies_file,  # Use cookie file if required for authentication
