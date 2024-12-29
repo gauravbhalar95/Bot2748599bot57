4ffe187345a12fb83471484f658d76fdd8268d10
gauravbhalar95 <141895800+gauravbhalar95@users.noreply.github.com>
2024-12-29 18:54:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ if not os.path.exists(output_dir):
 logging.basicConfig(level=logging.DEBUG)
 
 # Supported domains
-SUPPORTED_DOMAINS = ['youtube.com', 'youtu.be', 'instagram.com', 'x.com', 'facebook.com', 'xvideo.com', 'xnxx.com']
+SUPPORTED_DOMAINS = ['youtube.com', 'youtu.be', 'instagram.com', 'x.com', 'facebook.com']
 
 # Mega client
 mega_client = None
